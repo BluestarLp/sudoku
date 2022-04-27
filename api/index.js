95c2fe1dd224371484b5472c757245f1b8902d58
BluestarLp <87875238+BluestarLp@users.noreply.github.com>
2022-04-27 18:28:16 +0200

    Port weg

--- api/index.js
+++ api/index.js
@@ -4,5 +4,3 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
-app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
