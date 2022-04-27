098341fbcb0ad731f32185d927871ecc4f361c36
BluestarLp <87875238+BluestarLp@users.noreply.github.com>
2022-04-27 18:55:10 +0200

    asdf

--- api/index.js
+++ api/index.js
@@ -1,4 +1,7 @@
+import { fstat } from "fs";
+
 export default function handler(req, res) {
+  fs.writeFileSync("index.html", "<h1>Hello World</h1>");
   res.status(200).json({
     message: 'Welcome to the API',
   });
