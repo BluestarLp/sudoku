5b3871533942a4050dc5947c1bfc720fba34b317
BluestarLp <87875238+BluestarLp@users.noreply.github.com>
2022-04-27 18:04:28 +0200

    test

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
